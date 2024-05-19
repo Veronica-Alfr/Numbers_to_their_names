762903f9eab30f10f8664f3f6d9629770e5373fd
Veronica-Alfr <veagalves@gmail.com>
2024-05-19 10:37:38 -0300

    fix: add host to AWS IP public and logic to host local or host AWS

--- NumbersToTheirNames.js
+++ NumbersToTheirNames.js
@@ -1,8 +1,13 @@
 import express, { json } from "express";
 import extenso from "extenso";
+import dotenv from "dotenv";
 
 const app = express();
 const port = 3000;
+
+dotenv.config();
+
+const host = process.env.HOST || "localhost";
 
 app.use(json());
 
@@ -30,5 +35,5 @@ app.post("/convert", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://${host}:${port}`);
 });
